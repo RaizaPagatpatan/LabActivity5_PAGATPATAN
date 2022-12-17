89261d5e0afebd2239d4fc5e651ae0aab80dc72a
Raiza Pagatpatan <rjimenezpagatpatan@gmail.com>
2022-12-17 17:27:25 +0800

    Complete SimpleCalcGUI

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,28 +1,44 @@
 package SimpleCalc;
 
-import LeapYear.LeapYearGUI;
-
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Objects;
 
-public class SimpleCalcGUI {
+public class SimpleCalcGUI extends  JFrame{
     private JPanel panel1;
     private JTextField tfNumber1;
     private JTextField tfNumber2;
     private JComboBox cbOperations;
     private JButton btnCompute;
-    private JLabel lblResult;
+    private JTextField lblResult;
 
 
     public SimpleCalcGUI() {
         btnCompute.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            int num1 = Integer.parseInt(tfNumber1.getText());
-            int num2 = Integer.parseInt(tfNumber2.getText());
 
-            }
+                    int num1 = Integer.parseInt(tfNumber1.getText());
+                    int num2 = Integer.parseInt(tfNumber2.getText());
+
+                    if (Objects.equals(cbOperations.getSelectedItem(), "+")) {
+                        int add = num1 + num2;
+                        lblResult.setText(String.valueOf(add));
+                    } else if (Objects.equals(cbOperations.getSelectedItem(), "-")) {
+                        int minus = num1 - num2;
+                        lblResult.setText(String.valueOf(minus));
+                    } else if (Objects.equals(cbOperations.getSelectedItem(), "*")) {
+                        int multi = num1 * num2;
+                        lblResult.setText(String.valueOf(multi));
+                    } else if (Objects.equals(cbOperations.getSelectedItem(), "/")) {
+                        int div = num1 / num2;
+                        lblResult.setText(String.valueOf(div));
+                    }
+                }
+
+
+
         });
     }
 
@@ -33,6 +49,7 @@ public class SimpleCalcGUI {
         leapFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         leapFrame.pack();
         leapFrame.setVisible(true);
+
 
 
     }
