372e1d257c5fe89455a053293646b55b576e6aae
Raiza Pagatpatan <rjimenezpagatpatan@gmail.com>
2022-12-17 19:19:58 +0800

    Complete FoodOrderGUI

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,8 +1,8 @@
 package SimpleCalc;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
 import java.util.Objects;
 
 public class SimpleCalcGUI extends  JFrame{
@@ -15,37 +15,31 @@ public class SimpleCalcGUI extends  JFrame{
 
 
     public SimpleCalcGUI() {
-        btnCompute.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    int num1 = Integer.parseInt(tfNumber1.getText());
-                    int num2 = Integer.parseInt(tfNumber2.getText());
+        btnCompute.addActionListener(e -> {
+            try {
+                int num1 = Integer.parseInt(tfNumber1.getText());
+                int num2 = Integer.parseInt(tfNumber2.getText());
 
-                    if (Objects.equals(cbOperations.getSelectedItem(), "+")) {
-                        int add = num1 + num2;
-                        lblResult.setText(String.valueOf(add));
-                    } else if (Objects.equals(cbOperations.getSelectedItem(), "-")) {
-                        int minus = num1 - num2;
-                        lblResult.setText(String.valueOf(minus));
-                    } else if (Objects.equals(cbOperations.getSelectedItem(), "*")) {
-                        int multi = num1 * num2;
-                        lblResult.setText(String.valueOf(multi));
-                    } else if (Objects.equals(cbOperations.getSelectedItem(), "/")) {
-                        int div = num1 / num2;
-                        lblResult.setText(String.valueOf(div));
-                    }
-                }   catch (NumberFormatException e1){
-                    JOptionPane.showMessageDialog(null, "Enter Numeric Value!", null, JOptionPane.ERROR_MESSAGE);
+                if (Objects.equals(cbOperations.getSelectedItem(), "+")) {
+                    int add = num1 + num2;
+                    lblResult.setText(String.valueOf(add));
+                } else if (Objects.equals(cbOperations.getSelectedItem(), "-")) {
+                    int minus = num1 - num2;
+                    lblResult.setText(String.valueOf(minus));
+                } else if (Objects.equals(cbOperations.getSelectedItem(), "*")) {
+                    int multi = num1 * num2;
+                    lblResult.setText(String.valueOf(multi));
+                } else if (Objects.equals(cbOperations.getSelectedItem(), "/")) {
+                    int div = num1 / num2;
+                    lblResult.setText(String.valueOf(div));
                 }
-                    catch (ArithmeticException e2){
-                    JOptionPane.showMessageDialog(null, "Error", null, JOptionPane.ERROR_MESSAGE);
-                }
-                }
-
-
-
-        });
+            }   catch (NumberFormatException e1){
+                JOptionPane.showMessageDialog(null, "Enter Numeric Value!", null, JOptionPane.ERROR_MESSAGE);
+            }
+                catch (ArithmeticException e2){
+                JOptionPane.showMessageDialog(null, "Error", null, JOptionPane.ERROR_MESSAGE);
+            }
+            });
     }
 
     public static void main(String[] args) {
