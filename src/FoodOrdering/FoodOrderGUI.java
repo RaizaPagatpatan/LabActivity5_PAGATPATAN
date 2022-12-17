7b3a6ee5b7159f6a38d84c6e5cbb13a67de90c20
Raiza Pagatpatan <rjimenezpagatpatan@gmail.com>
2022-12-17 19:15:38 +0800

    Complete FoodOrderGUI

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,12 +1,14 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 
-public class FoodOrderGUI {
+public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
 
     private JCheckBox cPizza;
-    private JCheckBox cChula;
     private JCheckBox cBurger;
     private JCheckBox cFries;
     private JCheckBox cSoftDrinks;
@@ -20,6 +22,56 @@ public class FoodOrderGUI {
     private JRadioButton rb15;
 
     private JButton btnOrder;
+
+    public FoodOrderGUI() {
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DecimalFormat d = new DecimalFormat("0.00");
+
+                float amount = 0;
+                String msg = "";
+                if(cPizza.isSelected()){
+                    amount+=100;
+                }
+                if(cBurger.isSelected()){
+                    amount+=80;
+                }
+                if(cFries.isSelected()){
+                    amount+=65;
+                }
+                if(cSoftDrinks.isSelected()){
+                    amount+=55;
+                }
+                if(cTea.isSelected()){
+                    amount+=50;
+                }
+                if(cSundae.isSelected()){
+                    amount+=40;
+                }
+
+
+                if(rbNone.isSelected()) {
+                    JOptionPane.showMessageDialog(null,msg+"The total price is: "+ d.format(amount));}
+                else if(rb5.isSelected()) {
+                    JOptionPane.showMessageDialog(null,msg+"The total price is: "+ d.format(amount -(amount * .05)));}
+                else if(rb10.isSelected()) {
+                    JOptionPane.showMessageDialog(null,msg+"The total price is: "+ d.format(amount - (amount *.10)));}
+                else if(rb15.isSelected()) {
+                    JOptionPane.showMessageDialog(null,msg+"The total price is: "+ d.format(amount - (amount *.15))) ;}
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+//        FoodOrder Main Function
+        JFrame foodFrame = new JFrame("Food Ordering System");
+        foodFrame.setContentPane(new FoodOrderGUI().panel1);
+        foodFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        foodFrame.pack();
+        foodFrame.setVisible(true);
+    }
 
 
 }
