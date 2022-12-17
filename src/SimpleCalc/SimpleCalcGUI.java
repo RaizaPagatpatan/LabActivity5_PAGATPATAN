4dc28ac9670fc6a9a4f1d345c82fc405f59e895c
Raiza Pagatpatan <rjimenezpagatpatan@gmail.com>
2022-12-17 15:38:12 +0800

    Rename SimpleCalcGUI.java and SimpleCalcGUI.form components

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,10 +1,39 @@
 package SimpleCalc;
 
+import LeapYear.LeapYearGUI;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI {
     private JPanel panel1;
-    private JTextField textField1;
-    private JComboBox comboBox1;
-    private JButton computeResultButton;
+    private JTextField tfNumber1;
+    private JTextField tfNumber2;
+    private JComboBox cbOperations;
+    private JButton btnCompute;
+    private JLabel lblResult;
+
+
+    public SimpleCalcGUI() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            int num1 = Integer.parseInt(tfNumber1.getText());
+            int num2 = Integer.parseInt(tfNumber2.getText());
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+//       Simple calc
+        JFrame leapFrame = new JFrame("Simple Calculator");
+        leapFrame.setContentPane(new SimpleCalcGUI().panel1);
+        leapFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        leapFrame.pack();
+        leapFrame.setVisible(true);
+
+
+    }
 }
