ef1780e59d2c471c9ee78ab02ac008019e87a42f
Raiza Pagatpatan <rjimenezpagatpatan@gmail.com>
2022-12-17 11:51:54 +0800

    Leap Year Extend JFrame Class Main

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,20 +1,34 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LeapYearGUI {
 
     private JPanel panel1;
 
     private JTextField tfYear;
-
     private JButton btnCheckYear;
 
     private JOptionPane LeapYear;
 
-    public static void main(String[] args) {
-        LeapYearGUI app = new LeapYearGUI();
+    public LeapYearGUI() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+    }
+
+  public static void main(String[] args) {
+//         LeapYearGUI app = new LeapYearGUI();
+        JFrame leapFrame = new JFrame("Leap Year Checker");
+        leapFrame.setContentPane(new LeapYearGUI().panel1);
+        leapFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        leapFrame.pack();
+        leapFrame.setVisible(true);
     }
 
 }
